aaf29e4d3674e41c1e4250a73c3c4296e05294c5
Lucius Holzinger <luciholzinger@gmail.com>
2025-09-11 16:48:40 -0700

    header setup. now go

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import Demo from "./components/Demo";
+import { Container, Theme } from "@chakra-ui/react";
 import { Provider } from "./components/ui/provider";
+// import Demo from "./components/Demo";
+import Header from "./components/Header";
 
 function App() {
   return (
     <Provider>
-      <Demo />
+      <Theme appearance="light">
+        <Container bg={"#fff"} maxW={"full"} py={6} px={6}>
+          <Header />
+        </Container>
+      </Theme>
     </Provider>
   );
 }
