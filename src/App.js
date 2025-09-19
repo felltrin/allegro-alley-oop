89e043397d8811c083aa3e3f62bcb213fd2ca45d
Lucius Holzinger <luciholzinger@gmail.com>
2025-09-19 04:35:29 -0700

    the sidebar is now populated with the content according to the design for consistency
    Signed-off-by: Lucius Holzinger <luciholzinger@gmail.com>

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import { Provider } from "./components/ui/provider";
 // import Demo from "./components/Demo";
 import Header from "./components/Header";
 import { FaMagnifyingGlass } from "react-icons/fa6";
+import { FaRegCalendarAlt, FaRegFileAlt } from "react-icons/fa";
+import { IoChatbubbleEllipsesOutline } from "react-icons/io5";
+import { TbDeviceProjector } from "react-icons/tb";
+import { LuHouse, LuNotebookText } from "react-icons/lu";
+import { RiContactsBook2Line } from "react-icons/ri";
+import { TiClipboard } from "react-icons/ti";
+import { FiShoppingCart } from "react-icons/fi";
+import { SlEnvolope } from "react-icons/sl";
 
 function App() {
   return (
@@ -25,6 +33,71 @@ function App() {
               <Flex alignItems={"center"} gap={2}>
                 <FaMagnifyingGlass />
                 Search anything
+              </Flex>
+            </Box>
+            <Container pt={6} px={2}>
+              <Text color={"gray.400"} textStyle={"xs"}>
+                MAIN MENU
+              </Text>
+            </Container>
+            <Box p={2}>
+              <Flex alignItems={"center"} gap={2}>
+                <LuHouse />
+                Dashboard
+              </Flex>
+            </Box>
+            <Box p={2}>
+              <Flex alignItems={"center"} gap={2}>
+                <TiClipboard />
+                Task
+              </Flex>
+            </Box>
+            <Box p={2}>
+              <Flex alignItems={"center"} gap={2}>
+                <FiShoppingCart />
+                E-Commerce
+              </Flex>
+            </Box>
+            <Box p={2}>
+              <Flex alignItems={"center"} gap={2}>
+                <FaRegCalendarAlt />
+                Calendar
+              </Flex>
+            </Box>
+            <Box p={2}>
+              <Flex alignItems={"center"} gap={2}>
+                <SlEnvolope />
+                Mail
+              </Flex>
+            </Box>
+            <Box p={2}>
+              <Flex alignItems={"center"} gap={2}>
+                <IoChatbubbleEllipsesOutline />
+                Chat
+              </Flex>
+            </Box>
+            <Box p={2}>
+              <Flex alignItems={"center"} gap={2}>
+                <TbDeviceProjector />
+                Projects
+              </Flex>
+            </Box>
+            <Box p={2}>
+              <Flex alignItems={"center"} gap={2}>
+                <FaRegFileAlt />
+                File Manager
+              </Flex>
+            </Box>
+            <Box p={2}>
+              <Flex alignItems={"center"} gap={2}>
+                <LuNotebookText />
+                Notes
+              </Flex>
+            </Box>
+            <Box p={2}>
+              <Flex alignItems={"center"} gap={2}>
+                <RiContactsBook2Line />
+                Contacts
               </Flex>
             </Box>
           </Container>
