2f167081e7dd3b7dfaa99dbe0ce1dd6e5f59cc53
Lucius Holzinger <luciholzinger@gmail.com>
2025-09-19 03:33:50 -0700

    the sidebar search bar has been implemented and styled to the design for consistency
    Signed-off-by: Lucius Holzinger <luciholzinger@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import { Container, Flex, Text, Theme } from "@chakra-ui/react";
+import { Box, Container, Flex, Text, Theme } from "@chakra-ui/react";
 import { Provider } from "./components/ui/provider";
 // import Demo from "./components/Demo";
 import Header from "./components/Header";
+import { FaMagnifyingGlass } from "react-icons/fa6";
 
 function App() {
   return (
@@ -10,14 +11,29 @@ function App() {
         <Container bg={"#fff"} maxW={"full"} py={2} px={6}>
           <Header />
         </Container>
-        <Flex>
-          <Container bg={"#fff"} minH={"95vh"} maxW={"24vh"}></Container>
+        <Flex minH={"93.3dvh"}>
+          {/* Sidebar */}
+          <Container bg={"#fff"} maxW={"24vh"} px={2} pt={4}>
+            {/* <Demo /> */}
+            <Box
+              bg={"gray.100"}
+              w={"100%"}
+              p={2}
+              color={"gray.600"}
+              rounded={"sm"}
+            >
+              <Flex alignItems={"center"} gap={2}>
+                <FaMagnifyingGlass />
+                Search anything
+              </Flex>
+            </Box>
+          </Container>
+          {/* Main */}
           <Container
-            bg={"gray.200"}
-            pl={8}
-            py={12}
-            minH={"95vh"}
-            maxW={"170vh"}
+            bg={"gray.100"}
+            px={8}
+            py={10}
+            maxW={"100%"}
             border={"2px solid"}
             borderColor={"gray.200"}
           >
