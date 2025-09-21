0c5bec43b6021740b554635244f59f889477f22f
Lucius Holzinger <luciholzinger@gmail.com>
2025-09-20 18:29:09 -0700

    the metric cards are implemented and styled according to the design for consistency
    Signed-off-by: Lucius Holzinger <luciholzinger@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import {
   Box,
   Button,
+  Card,
   Container,
   Flex,
-  // Icon,
+  IconButton,
+  Icon,
   Menu,
   Portal,
+  Stack,
   Text,
   Theme,
 } from "@chakra-ui/react";
@@ -14,7 +17,10 @@ import { Provider } from "./components/ui/provider";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import { GoDownload } from "react-icons/go";
-import { BiChevronDown } from "react-icons/bi";
+import { BiChevronDown, BiUser } from "react-icons/bi";
+import { FaDollarSign } from "react-icons/fa6";
+import { LuChartColumnBig } from "react-icons/lu";
+import { BsArrowDown, BsArrowUp } from "react-icons/bs";
 
 function App() {
   return (
@@ -35,7 +41,7 @@ function App() {
             border={"2px solid"}
             borderColor={"gray.200"}
           >
-            <Flex alignItems={"center"} justifyContent={"space-between"}>
+            <Flex alignItems={"center"} justifyContent={"space-between"} pb={8}>
               <Flex>
                 <Text textStyle={"3xl"} fontWeight={"semibold"}>
                   Overview
@@ -83,6 +89,121 @@ function App() {
                   </Portal>
                 </Menu.Root>
               </Flex>
+            </Flex>
+            <Flex justifyContent={"space-between"}>
+              <Card.Root
+                border={"1px solid"}
+                borderColor={"gray.200"}
+                minW={"470px"}
+              >
+                <Card.Body>
+                  <Flex alignItems={"center"} justifyContent={"space-between"}>
+                    <Stack>
+                      <Text textStyle={"sm"} color={"gray.400"}>
+                        Total Profit
+                      </Text>
+                      <Flex alignItems={"center"} gap={2}>
+                        <Text textStyle={"3xl"} fontWeight={"semibold"}>
+                          $14.500
+                        </Text>
+                        <Flex alignItems={"center"}>
+                          <Icon size={12} color={"green.500"}>
+                            <BsArrowUp />
+                          </Icon>
+
+                          <Text fontWeight={"semibold"} color={"green.500"}>
+                            50.8%
+                          </Text>
+                        </Flex>
+                      </Flex>
+                    </Stack>
+                    <IconButton
+                      bg={"#ecf3e79c"}
+                      size={"2xl"}
+                      rounded={"3xl"}
+                      pointerEvents={"none"}
+                    >
+                      <FaDollarSign color={"#B8EB9E"} />
+                    </IconButton>
+                  </Flex>
+                </Card.Body>
+              </Card.Root>
+              <Card.Root
+                border={"1px solid"}
+                borderColor={"gray.200"}
+                minW={"470px"}
+              >
+                <Card.Body>
+                  <Flex alignItems={"center"} justifyContent={"space-between"}>
+                    <Stack>
+                      <Text textStyle={"sm"} color={"gray.400"}>
+                        Total Sales
+                      </Text>
+
+                      <Flex alignItems={"center"} gap={2}>
+                        <Text textStyle={"3xl"} fontWeight={"semibold"}>
+                          26.800K
+                        </Text>
+                        <Flex alignItems={"center"}>
+                          <Icon size={12} color={"red.400"}>
+                            <BsArrowDown />
+                          </Icon>
+
+                          <Text fontWeight={"semibold"} color={"red.400"}>
+                            10.5%
+                          </Text>
+                        </Flex>
+                      </Flex>
+                    </Stack>
+                    <IconButton
+                      bg={"#ecf3e79c"}
+                      size={"2xl"}
+                      rounded={"3xl"}
+                      pointerEvents={"none"}
+                    >
+                      <LuChartColumnBig color={"#B8EB9E"} />
+                    </IconButton>
+                  </Flex>
+                </Card.Body>
+              </Card.Root>
+              <Card.Root
+                border={"1px solid"}
+                borderColor={"gray.200"}
+                minW={"470px"}
+              >
+                <Card.Body>
+                  <Flex alignItems={"center"} justifyContent={"space-between"}>
+                    <Stack>
+                      <Text textStyle={"sm"} color={"gray.400"}>
+                        New Users
+                      </Text>
+
+                      <Flex alignItems={"center"} gap={2}>
+                        <Text textStyle={"3xl"} fontWeight={"semibold"}>
+                          8.300K
+                        </Text>
+                        <Flex alignItems={"center"}>
+                          <Icon size={12} color={"green.500"}>
+                            <BsArrowUp />
+                          </Icon>
+
+                          <Text fontWeight={"semibold"} color={"green.500"}>
+                            24.9%
+                          </Text>
+                        </Flex>
+                      </Flex>
+                    </Stack>
+                    <IconButton
+                      bg={"#ecf3e79c"}
+                      size={"2xl"}
+                      rounded={"3xl"}
+                      pointerEvents={"none"}
+                    >
+                      <BiUser color={"#B8EB9E"} />
+                    </IconButton>
+                  </Flex>
+                </Card.Body>
+              </Card.Root>
             </Flex>
           </Container>
         </Flex>
